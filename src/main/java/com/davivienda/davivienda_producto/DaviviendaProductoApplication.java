1fd619807f81b04fedf3764d1cdaf6fb34c59c8f
gaspar <gaspar@pizzati.com>
2025-01-14 10:14:23 -0600

    Inicio

---
+++ src/main/java/com/davivienda/davivienda_producto/DaviviendaProductoApplication.java
@@ -0,0 +1,13 @@
+package com.davivienda.davivienda_producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaviviendaProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaviviendaProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davivienda/davivienda_producto/DaviviendaProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davivienda.davivienda_producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaviviendaProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
